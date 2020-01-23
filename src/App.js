d8a3ea287424ed349785536a2a63fd00b8ae2cb1
Mathew Morris <hello@mathewmorris.com>
2020-01-22 23:57:22 -0600

    Fix prettier

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React, { useState } from "react";
-import { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme } from "./theme";
-import { GlobalStyles } from "./global";
+import React, { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './theme';
+import { GlobalStyles } from './global';
 
 function App() {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState('light');
 
   // The function that toggles between themes
   const toggleTheme = () => {
     // if the theme is not light, then set it to dark
-    if (theme === "light") {
-      setTheme("dark");
+    if (theme === 'light') {
+      setTheme('dark');
       // otherwise, it should be light
     } else {
-      setTheme("light");
+      setTheme('light');
     }
   };
 
   return (
-    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
       <>
         <GlobalStyles />
         <button onClick={toggleTheme}>Toggle theme</button>
